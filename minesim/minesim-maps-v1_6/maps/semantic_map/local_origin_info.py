52453ab3d4d6ff0a68ab38017fcf28b2ece0dcb8
Zhifa Chen <chenzf2013@163.com>
2024-11-26 18:04:01 +0800

    add MineSim maps version 1.6

---
+++ minesim/minesim-maps-v1_6/maps/semantic_map/local_origin_info.py
@@ -0,0 +1,56 @@
+
+
+# ! 注意：需要托密或者开源数据,直接扫除掉该文件`local_origin_info.py`
+# 广东大排矿区
+{
+	"version":"1.5",
+	"map_make_date":"2023-12-16",
+	"infomation_of_map":"modify reference path and borderline of jiangtong map using matlab",
+	"CoordinateReferenceSystem":{
+		"CRS_name":"GCS_WGS_1984_UTM_Zone_49N",
+		"unit":"m"
+	},
+	"local_origin_utm_xy":[559428.1239,2580653.674],
+	"local_origin_lonlat":[0,0],
+	"bitmap_rgb_PNG":{
+		"png_version":"rgb_20231111.png,20231111",
+		"UTM_info":{
+			"point_southwest":[559428.1239,2580653.674],
+			"point_northeast":[562590.1239,2582587.674],
+			"local_x_range":[0,3160.6722],
+			"local_y_range":[0,1933.2115],
+			"unit":"m"
+		},
+		"canvas_edge_meter":[3162,1934],
+		"canvas_edge_pixel":[6324,3868],
+		"scale_PixelPerMeter":2,
+		"scale_MeterPerPixel":0.5
+	},
+}
+
+# 江西江铜矿区
+{
+	"version":"1.5",
+	"map_make_date":"2023-10-10",
+	"infomation_of_map":"modify reference path and borderline of jiangtong map using matlab",
+	"CoordinateReferenceSystem":{
+		"CRS_name":"GCS_WGS_1984_UTM_Zone_50N",
+		"unit":"m"
+	},
+	"local_origin_utm_xy":[572476.7689,3205292.032],
+	"local_origin_lonlat":[71.0578537,42.33684917],
+	"bitmap_rgb_PNG":{
+		"png_version":"fujiawu1_tif2png-0dot5.png,20230603",
+		"UTM_info":{
+			"point_southwest":[572476.7689,3205292.032],
+			"point_northeast":[572476.7689+1824.5,3205292.032+4009.5],
+			"local_x_range":[0,1824.5],
+			"local_y_range":[0,4009.5],
+			"unit":"m"
+		},
+		"canvas_edge_meter":[1824.5,4009.5],
+		"canvas_edge_pixel":[3649,8019],
+		"scale_PixelPerMeter":2,
+		"scale_MeterPerPixel":0.5
+	},
+ }
